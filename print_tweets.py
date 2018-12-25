63e1457175a37975fbc3355bca64a89361d8d35b
hamdan92 <7mdan92@gmail.com>
2018-12-25 16:23:26 +0400

    utf-8 support

--- print_tweets.py
+++ print_tweets.py
@@ -6,4 +6,4 @@ with open('tweets.csv') as csv_file:
   csv_reader=csv.reader(csv_file,delimiter=',')
   line_count=0
   for row in csv_reader:
-    print (row[1])
+    print (row[1].decode('utf-8'))
